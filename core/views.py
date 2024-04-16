3c3347c5cb27eb8bdbe43e2f2bb7fdf00bcf2200
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2024-04-16 15:01:26 +0330

    📎 Create login Authentication views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 
-# Create your views here.
+# login Authenticate
+class Login(ObtainAuthToken):
+    def post(self, request, *args,**kwargs):
+        serializers = self.get_serializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        user = serializers.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, "user_id": user.id})
+    
