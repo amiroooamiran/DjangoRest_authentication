a7d39154a4de5b9a7159f4110185b5314b6e22e1
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2024-04-16 18:47:43 +0330

    ♻️ config url path

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import Login
 
 urlpatterns = [
-    path('login', Login.as_view(), name='login')    
+    path('login/', Login.as_view(), name='login')    
 ]
