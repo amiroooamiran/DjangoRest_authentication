e511d28b0eb0325f3c484dd6cc77246022bf1a66
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2024-04-16 13:53:32 +0330

    ➕ add core urls in the project url

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+
     # Configure urls.py. Pay attention to djoser.url.authtoken module path:
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+
+    # apps urls
+    path('', include('core.urls'))
 ]
