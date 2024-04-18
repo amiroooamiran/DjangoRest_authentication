b33b60e007d02f9d3d4ec1a7d67f2ca6d85ebd0d
AmirhosseinAmiran <amiran.amirhossein@gmail.com>
2024-04-18 22:54:13 +0330

    ♻️ Conifg RegisterSerializer & ProfileSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -5,22 +5,42 @@ from .models import Profile
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']  # You can add more fields if needed
+        fields = ['id', 'username', 'email']
 
 class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
 
 class ProfileSerializer(serializers.ModelSerializer):
-    user = UserSerializer(required=True)  # Specify user as a required field
+    user = UserSerializer()
 
     class Meta:
         model = Profile
-        fields = "__all__"
+        fields = ['user', 'profile_pic', 'bio', 'joine_date']
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        user = User.objects.create(**user_data)
+        profile = Profile.objects.create(user=user, **validated_data)
+        return profile
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user')
+        user = instance.user
+        user.username = user_data.get('username', user.username)
+        user.email = user_data.get('email', user.email)
+        user.save()
+
+        instance.profile_pic = validated_data.get('profile_pic', instance.profile_pic)
+        instance.bio = validated_data.get('bio', instance.bio)
+        instance.save()
+
+        return instance
 
