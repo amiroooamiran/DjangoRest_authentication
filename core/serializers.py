1fac2e7d59f24f1e36e0f7d6b29d0ad32394a7fc
amiroooamiran <amirhosseinamiran2004@gmail.com>
2024-04-18 09:34:04 -0400

    📎 create User Registration Serializers

--- core/serializers.py
+++ core/serializers.py
@@ -7,4 +7,14 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email']  # You can add more fields if needed
 
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
 
