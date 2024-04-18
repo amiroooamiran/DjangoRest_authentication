d5d8c905bfb38471d8c9584c06600219affb0470
amiroooamiran <amirhosseinamiran2004@gmail.com>
2024-04-18 09:33:27 -0400

    📎 Create User Serializers

---
+++ core/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Profile
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']  # You can add more fields if needed
+
+
