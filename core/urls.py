af42702ee19d6d46bf9f82752dc7051e4b680337
amiroooamiran <amirhosseinamiran2004@gmail.com>
2024-04-18 09:35:59 -0400

    📎 Create Profile Urls

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from .views import Login, ProfileView, Register
 urlpatterns = [
     path('register/', Register.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
-
+    path('profile/', ProfileView.as_view(), name='profile')
 ]
