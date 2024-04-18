c35d4c5015750d17c65d5411f535a5ad45bd1190
amiroooamiran <amirhosseinamiran2004@gmail.com>
2024-04-18 09:32:34 -0400

    📎 Create Url of user Registration

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import Login
+from .views import Login, ProfileView, Register
 
 urlpatterns = [
-    path('login/', Login.as_view(), name='login')    
+    path('register/', Register.as_view(), name='register'),
+    path('login/', Login.as_view(), name='login'),
+
 ]
