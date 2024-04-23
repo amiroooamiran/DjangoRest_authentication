22c315873f0e68978742e6a84152f48d216d8ca1
amirooo amiran <amiran.amirhossein@gmail.com>
2024-04-23 17:00:38 +0330

    📎 add verficiation views in urls

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import Login, ProfileView, Register
+from .views import Login, ProfileView, RegisterView, VerifyEmail
 
 urlpatterns = [
-    path('register/', Register.as_view(), name='register'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
-    path('profile/', ProfileView.as_view(), name='profile')
+    path('profile/', ProfileView.as_view(), name='profile'),
+    path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
 ]
