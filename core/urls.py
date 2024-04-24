4aaa902ebce4f3bbaf622f5fafb95fadb5646241
amirooo amiran <amiran.amirhossein@gmail.com>
2024-04-24 14:39:12 +0330

    🖇️ Create Urls of reset password

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import Login, ProfileView, RegisterView, VerifyEmail
+from .views import Login, ProfileView, RegisterView, VerifyEmail, ResetPassword
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', Login.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
+    path('reset_password/', ResetPassword.as_view(), name="reset_password")
 ]
