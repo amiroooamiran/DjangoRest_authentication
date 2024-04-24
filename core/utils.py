67d353ce8ee8b09bd653fea8d30e62d41b2b06ae
amirooo amiran <amiran.amirhossein@gmail.com>
2024-04-24 14:38:34 +0330

    📎 create generate random password in Utils

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,6 @@
 from django.core.mail import EmailMessage
+import random
+import string
 
 class Util:
     @staticmethod
@@ -6,3 +8,10 @@ class Util:
         email = EmailMessage(
             subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
         email.send()
+    
+    @staticmethod
+    def generate_random_password():
+        # Generate a random password using uppercase letters, lowercase letters, digits, and special characters
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choices(characters, k=12))  # Generate a 12-character password
+        return password
