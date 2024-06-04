68d45260761f119c9a3cf1cfab40ccd8cfbdbe64
amirooo amiran <amiran.amirhossein@gmail.com>
2024-06-04 22:32:40 +0330

    🔄 create custom django command for launch qeueue listener

---
+++ core/management/commands/launch_queue_listener.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+from core.queue_listener import UserCreatedListener
+
+class Command(BaseCommand):
+    help = 'Launches Listener for user_created message : Kafka'
+    def handle(self, *args, **options):
+        td = UserCreatedListener()
+        td.start()
+        self.stdout.write("Started Consumer Thread")
--- core/management/commands/welcome.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand
-class Command(BaseCommand):
-    # python3 manage.py help welcome
-    help = 'Just saying Welcome to django users'  
-    
-    def handle(self, *args, **kwargs):
-        self.stdout.write("Welcome to django commands")
